ecd0a7948fcf790b63a2c1b41693805eec3ff7ec
HozumiRyo <oibc35243@ojs.ac.jp>
2024-04-24 15:51:29 +0900

    2-1

---
+++ Chapter02/演習問題/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var so1 = new Song("タイムパラドックス", "Vaundy", 227);
+            var so2 = new Song("アイドル", "yoasobi", 214);
+            var so3 = new Song("Pretender", "Official髭男dism", 326);
+            Song[] songs = new Song[] { so1, so2, so3 };
+            PrintSongs(songs);
+
+            
+            }
+            private static void PrintSongs(Song[] songs) {
+                for (int i = 0; i < songs.Length; i++) {
+                Console.WriteLine(songs[i].Title +":"+ songs[i].ArtistName +":"+ @"{0:mm\:ss}", TimeSpan.FromSeconds(songs[i].Length));
+            }
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise1/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    internal class Song {
+    public String Title { get; set; }
+    public String ArtistName { get; set; }
+    public int Length { get; set; }
+
+        public Song(String Title,String ArtistName,int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+    }
+}

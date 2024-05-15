8049ed2baa0737e7403ba6e80e4c761a96a7016b
HozumiRyo <oibc35243@ojs.ac.jp>
2024-05-15 14:58:24 +0900

    問題3.1完成

--- Chapter03/演習問題/Exercize01/Program.cs
+++ Chapter03/演習問題/Exercize01/Program.cs
@@ -7,6 +7,46 @@ using System.Threading.Tasks;
 namespace Exercize01 {
     internal class Program {
         static void Main(string[] args) {
+
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
+            var exists = numbers.Exists(s => s % 8 == 0 ^ s % 9 == 0);
+            Console.WriteLine(exists);
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(x => Console.WriteLine(x/2));
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+            IEnumerable<int>query = numbers.Where(x => x >= 50); 
+            foreach (int x in query) {
+                Console.WriteLine(x);
+            }
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+            var query = numbers.Select(s => s * 2);
+            foreach(int x in query) {
+                Console.WriteLine(x);
+            }
         }
     }
 }

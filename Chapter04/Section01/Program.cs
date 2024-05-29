59f4df30faf40b5489e201872ef38aee81f50e67
HozumiRyo <oibc35243@ojs.ac.jp>
2024-05-29 10:02:41 +0900

    4章終了

---
+++ Chapter04/Section01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            #region null合体演算子
+
+            String code = "12345";
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+
+            #endregion
+
+            #region null条件演算子
+            //Sale sale = new Sale() {
+            //    Amount = 1000,
+            //};
+            Sale sale = null;
+
+            //「int?」はnull許容型、「?.」はnull条件演算子
+            //int? ret = sale?.Amount;
+            //Console.WriteLine(ret);
+            #endregion
+
+
+            Console.Write("整数を入力:");
+            string inputNum = Console.ReadLine();
+
+            try {
+                int num = int.Parse(inputNum);
+            }
+            catch(FormatException ex) {
+                Console.WriteLine("ArgumentEzception" + ex.Message);
+            }
+            catch (ArgumentException ex) {
+                Console.WriteLine("ArgumentEzception" + ex.Message);
+
+            }
+            catch (OverflowException ex){
+                Console.WriteLine("ArgumentEzception" + ex.Message);
+            }
+            finally {
+                Console.WriteLine("処理が終了しました。");
+            }
+        }
+
+
+        private static object GetMessage(string code) {
+            return null;
+        }
+        private static object DefaultMessage() {
+            return "Default Message";
+        }
+    }
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //商品カテゴリ
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+    }
+}

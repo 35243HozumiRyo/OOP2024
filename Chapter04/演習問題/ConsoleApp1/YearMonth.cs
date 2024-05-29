f4da2d0d79548bff6e9897e11a9ea2459847174c
HozumiRyo <oibc35243@ojs.ac.jp>
2024-05-29 15:45:25 +0900

    演習問題5.1

--- Chapter04/演習問題/ConsoleApp1/YearMonth.cs
+++ Chapter04/演習問題/ConsoleApp1/YearMonth.cs
@@ -13,30 +13,31 @@ namespace Exercise01 {
         public int Month { get; private set; }
 
         public YearMonth(int year, int month) {
-            Year = year;
-            Month = month;
+            this.Year = year;
+            this.Month = month;
         }
 
         //4.1.2
         public bool Is21Century {
             get {
-                return 2001 <= Year && Year <= 2100;
+                return 2001 <= this.Year && this.Year <= 2100;
 
             }
         }
 
         //4.1.3
         public YearMonth AddOneMonth() {
-            if (Month == 12) {
-                return new YearMonth(Year + 1, 1);
+            if (this.Month == 12) {
+                return new YearMonth(this.Year + 1, 1);
             } else {
-                return new YearMonth(Year + 1, Month + 1); ;
+                return new YearMonth(this.Year + 1, this.Month + 1); ;
             }
         }
 
         //4.1.4
         public override string ToString() {
-            return Year + "年" + Month+"月";
+            //return Year + "年" + Month + "月"
+            return $"{this.Year}年{this.Month}月";
         }
     }
 }
---
+++ Chapter05/Exercis01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercis01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("文字列を入力:");
+            var s1 = Console.ReadLine();
+            Console.Write("文字列を入力:");
+            var s2 = Console.ReadLine();
+
+            if (String.Compare(s1,s2,true) == 0)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
+        }
+    }
+}

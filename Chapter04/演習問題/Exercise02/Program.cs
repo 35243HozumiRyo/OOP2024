9615873b20a16bc3fe857c7d778b41ec451b4c38
HozumiRyo <oibc35243@ojs.ac.jp>
2024-05-29 11:41:09 +0900

    演習問題4.1

---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+    new YearMonth(1980, 1),
+    new YearMonth(1990, 4),
+    new YearMonth(2000, 7),
+    new YearMonth(2010, 9),
+    new YearMonth(2020, 12),
+};
+
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            throw new NotImplementedException();
+        }
+    }
+}

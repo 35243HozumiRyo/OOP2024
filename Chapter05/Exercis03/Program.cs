ea0c6a9db39d5323049f7152e80544dcaee0d80d
HozumiRyo <oibc35243@ojs.ac.jp>
2024-05-30 15:47:30 +0900

    5.5.5

--- Chapter05/Exercis03/Program.cs
+++ Chapter05/Exercis03/Program.cs
@@ -33,15 +33,25 @@ namespace Exercis03 {
         }
 
         private static void Exercise3_3(string text) {
-            Console.WriteLine(text.Length - text.Replace(' '.ToString(), "").Length + 1);
+            //Console.WriteLine(text.Length - text.Replace(' '.ToString(), "").Length + 1);
+            int count = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}",count );
         }
 
         private static void Exercise3_4(string text) {
-            
+            var words = text.Split(' ').Where(s=>s.Length<=4);
+            foreach(var word in words)
+                Console.WriteLine(word);
         }
 
         private static void Exercise3_5(string text) {
-            
+            var array =text.Split('　').ToArray();
+            var sb = new StringBuilder();
+            foreach(var word in array) {
+                sb.Append(word);
+                sb.Append(' ');
+            }
+            Console.WriteLine(sb);
         }
     }
 }

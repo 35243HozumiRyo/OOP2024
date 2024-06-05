6071eb4b1b6c67eccd25b1e8c9aa33a31d762880
HozumiRyo <oibc35243@ojs.ac.jp>
2024-06-05 10:23:36 +0900

    演習問題5.4

--- Chapter05/Exercis04/Program.cs
+++ Chapter05/Exercis04/Program.cs
@@ -8,14 +8,27 @@ namespace Exercise04 {
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-
+            foreach(var pair in line.Split(';')) {
+                var array = pair.Split('=');
+                Console.WriteLine("{0} : {1} ", ToJapanese(array[0]), array[1]);
+            }
 
         }
 
         //できたら以下のメソッドを完成させて利用する
-        //static string ToJapanese(string key) {
+       static string ToJapanese(string key) {
+            string ret = "";
 
+            switch (key) {
+                case "Novelist":
+                    return "作家;";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+            }
+            throw new ArgumentException("引数に誤りがあります");
 
-        //}
+        }
     }
 }

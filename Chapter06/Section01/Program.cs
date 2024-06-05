cd1082afd5186824abaa22d44a9d0fb9a1efdeb1
HozumiRyo <oibc35243@ojs.ac.jp>
2024-06-05 13:13:01 +0900

    第六章演習問題

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks().OrderByDescending(b=>b.Price).ToList();
+
+            books.ForEach(b => Console.WriteLine(b.Title));
+        }
+    }
+}

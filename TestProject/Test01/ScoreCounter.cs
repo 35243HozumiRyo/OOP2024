b2cf9a2082020fc0b68691a44abfafcfbff776bf
HozumiRyo <oibc35243@ojs.ac.jp>
2024-06-18 11:22:00 +0900

    設問１_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -13,20 +13,30 @@ namespace Test01 {
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-
-
-
-
-
+            List<Student> students = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] item = line.Split(',');
+                Student student = new Student {
+                    Name = item[0],
+                    Sbject = item[1],
+                    Score = int.Parse(item[2])
+                };
+                students.Add(student);
+            }
+            return students;
         }
-
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach (var student in _score) {
+                if (dict.ContainsKey(student.Sbject)) {
+                    dict[student.Sbject] += student.Score;
+                } else {
+                    dict[student.Sbject] = student.Score;
+                }
+            }
+            return dict;
         }
     }
 }
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,7 @@
 ﻿namespace Test01 {
     class Student {
-        string Name; //学生の名前
-        string Sbject; //科目
-        int Score; //点数
+        public string Name; //学生の名前
+        public string Sbject; //科目
+        public int Score; //点数
     }
 }

dd126a12afc6fd5f06e58ee9907a46bfd965cd79
HozumiRyo <oibc35243@ojs.ac.jp>
2024-09-18 11:53:13 +0900

    RssReader完成

--- FromApps/ADOSample/Form1.cs
+++ FromApps/ADOSample/Form1.cs
@@ -13,5 +13,18 @@ namespace ADOSample {
         public Form1() {
             InitializeComponent();
         }
+
+        private void booksBindingNavigatorSaveItem_Click(object sender, EventArgs e) {
+            this.Validate();
+            this.booksBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.infosys202413DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: このコード行はデータを 'infosys202413DataSet.Books' テーブルに読み込みます。必要に応じて移動、または削除をしてください。
+            this.booksTableAdapter.Fill(this.infosys202413DataSet.Books);
+
+        }
     }
 }

b004b4cdf39a1a0b7a0d318bea7a14a263808039
HozumiRyo <oibc35243@ojs.ac.jp>
2024-11-06 15:59:24 +0900

    SampleWeightUnitConverter 完成

---
+++ WPF/SampleWeightUnitConverter/Model/GramUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWeightUnitConverter {
+    //メートル単位を表すクラス
+    public class GramUnit : WeightUnit {
+        private static List<GramUnit> units = new List<GramUnit> {
+            new GramUnit{ Name = "g",Coefficient = 1,},
+            new GramUnit{ Name = "kg",Coefficient = 1*1000,},
+        };
+        public static ICollection<GramUnit> Units { get { return units; } }
+
+        /// <summary>
+        /// ヤード単位からメートル単位に変換します
+        /// </summary>
+        /// <param name="unit">ヤード単位</param>
+        /// <param name="value">値</param>
+        /// <returns></returns>
+        public double FromImperialUnit(ProundUnit unit, double value) {
+            return (value * unit.Coefficient) * 28.35 / this.Coefficient;
+        }
+    }
+
+}
---
+++ WPF/SampleWeightUnitConverter/Model/ProundUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWeightUnitConverter {
+    //ヤード単位を表すクラス
+    public class ProundUnit : WeightUnit {
+        private static List<ProundUnit> units = new List<ProundUnit> {
+            new ProundUnit{ Name = "oz",Coefficient = 1,},
+            new ProundUnit{ Name = "lb",Coefficient = 1 * 16,},
+        };
+        public static ICollection<ProundUnit> Units { get { return units; } }
+
+        /// <summary>
+        /// メートル単位からヤード単位に変換します
+        /// </summary>
+        /// <param name="unit">メートル単位</param>
+        /// <param name="value">値</param>
+        /// <returns></returns>
+        public double FromMetricUnit(GramUnit unit, double value) {
+            return (value * unit.Coefficient) / 28.35 / this.Coefficient;
+        }
+    }
+
+}

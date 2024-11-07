4984cb86a5075663af059e91c20de2c3fdc97a16
HozumiRyo <oibc35243@ojs.ac.jp>
2024-11-07 10:40:04 +0900

    stone の追加

--- WPF/SampleWeightUnitConverter/Model/ProundUnit.cs
+++ WPF/SampleWeightUnitConverter/Model/ProundUnit.cs
@@ -5,18 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SampleWeightUnitConverter {
-    //ヤード単位を表すクラス
+
     public class ProundUnit : WeightUnit {
         private static List<ProundUnit> units = new List<ProundUnit> {
             new ProundUnit{ Name = "oz",Coefficient = 1,},
             new ProundUnit{ Name = "lb",Coefficient = 1 * 16,},
+            new ProundUnit{ Name = "stone",Coefficient = 1 * 16 * 14,},
         };
         public static ICollection<ProundUnit> Units { get { return units; } }
 
         /// <summary>
-        /// メートル単位からヤード単位に変換します
+        /// グラム単位からOZ単位に変換します
         /// </summary>
-        /// <param name="unit">メートル単位</param>
+        /// <param name="unit">グラム単位</param>
         /// <param name="value">値</param>
         /// <returns></returns>
         public double FromMetricUnit(GramUnit unit, double value) {
